b17f7226e57e58bfbcfa3e9cd8a3dc1c051cc263
Marushkevych <marushkevych@gmail.com>
2024-05-16 17:14:40 -0400

    Converting the Users Slice to use createEntityAdapter

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { addNewPost } from './postsSlice'
+import { selectAllUsers } from '../users/usersSlice'
 
 export const AddPostForm = () => {
   const dispatch = useDispatch()
@@ -9,7 +10,7 @@ export const AddPostForm = () => {
   const [userId, setUserId] = useState('')
   const [addRequestStatus, setAddRequestStatus] = useState('idle')
 
-  const users = useSelector((state) => state.users)
+  const users = useSelector(selectAllUsers)
 
   const onTitleChanged = (e) => setTitle(e.target.value)
   const onContentChanged = (e) => setContent(e.target.value)
