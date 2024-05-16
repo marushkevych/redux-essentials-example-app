a67d814dd0a684ba01a22b3fb5198bd2706a30da
Marushkevych <marushkevych@gmail.com>
2024-05-16 16:52:46 -0400

    Optimizing the Posts List

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -5,10 +5,12 @@ import { Link } from 'react-router-dom'
 import { PostAuthor } from './PostAuthor'
 import TimeAgo from './TimeAgo'
 import { ReactionButtons } from './ReactionButtons'
-import { selectAllPosts, fetchPosts } from './postsSlice'
+import { selectPostIds, fetchPosts, selectPostById } from './postsSlice'
 import { Spinner } from '../../components/Spinner'
 
-const PostExcerpt = ({ post }) => {
+const PostExcerpt = ({ postId }) => {
+  const post = useSelector((state) => selectPostById(state, postId))
+
   return (
     <article className="post-excerpt">
       <h3>{post.title}</h3>
@@ -26,7 +28,7 @@ const PostExcerpt = ({ post }) => {
   )
 }
 export const PostsList = () => {
-  const posts = useSelector(selectAllPosts)
+  const orderedPostIds = useSelector(selectPostIds)
   const dispatch = useDispatch()
   const postStatus = useSelector((state) => state.posts.status)
   const error = useSelector((state) => state.posts.error)
@@ -42,13 +44,8 @@ export const PostsList = () => {
   if (postStatus === 'loading') {
     content = <Spinner text="Loading..." />
   } else if (postStatus === 'succeeded') {
-    // Sort posts in reverse chronological order by datetime string
-    const orderedPosts = posts
-      .slice()
-      .sort((a, b) => b.date.localeCompare(a.date))
-
-    content = orderedPosts.map((post) => {
-      return <PostExcerpt post={post} />
+    content = orderedPostIds.map((postId) => {
+      return <PostExcerpt key={postId} postId={postId} />
     })
   } else if (postStatus === 'failed') {
     content = <div className="error">{error}</div>
